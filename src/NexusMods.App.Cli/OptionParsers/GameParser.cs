54dba420d43af08c4b81353320f6dc131ef2dad0
axmanalad <alex.manalad8714@gmail.com>
2025-04-27 20:00:31 -0700

    Refactor to support parsing installation without requiring game

--- src/NexusMods.App.Cli/OptionParsers/GameParser.cs
+++ src/NexusMods.App.Cli/OptionParsers/GameParser.cs
@@ -11,10 +11,15 @@ internal class GameParser(IGameRegistry gameRegistry) : IOptionParser<IGame>
 {
     public bool TryParse(string toParse, out IGame value, out string error)
     {
-        var install = gameRegistry.Installations.Values.FirstOrDefault(g => g.Game.GameId.ToString() == toParse) ??
-                    gameRegistry.Installations.Values.FirstOrDefault(g => g.Game.Name.Equals(toParse, StringComparison.CurrentCultureIgnoreCase))!;
-
-        value = (IGame)install.Game;
+        var install = gameRegistry.SupportedGames.FirstOrDefault(g => g.GameId.ToString() == toParse) ??
+                    gameRegistry.SupportedGames.FirstOrDefault(g => g.Name.Equals(toParse, StringComparison.CurrentCultureIgnoreCase))!;
+        if (install == null!)
+        {
+            value = null!;
+            error = $"No game installation found matching '{toParse}'.\n";
+            return false;
+        }
+        value = (IGame)install;
         error = string.Empty;
         return true;
     }
